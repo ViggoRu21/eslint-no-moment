de245abedb37e0599d5bb66c2618972ad9266e3e
Glasabarn <viggoh21@ru.is>
2025-06-02 15:34:25 +0000

    Oopsie forgot to update package

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
-module.exports = {
-  rules: {
-    'no-moment': require('./src/lib/rules/no-moment')
-  },
-  configs: {
-    recommended: {
-      rules: {
-        'no-moment/no-moment': 'error'
-      }
+import noMoment from "./rules/no-moment"
+
+export const ruels = {
+  'no-moment': noMoment,
+};
+
+export const configs = {
+  recommended: {
+    plugins: ['no-moment'],
+    rules: {
+      'no-moment/no-moment': 'error' as const,
     }
   }
 };
