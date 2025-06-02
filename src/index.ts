1aabcafbe4295775618934667e1231fde5a4d2f7
Glasabarn <viggoh21@ru.is>
2025-06-02 16:20:50 +0000

    idk anymore

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
-import noMoment from "./rules/no-moment"
+import { TSESLint } from "@typescript-eslint/utils";
+import noMoment from "./rules/no-moment";
 
-export const rules = {
+// Thanks https://github.com/typescript-eslint/typescript-eslint/issues/7605
+export const rules: Record<string, TSESLint.RuleModule<string, unknown[]>> = {
   'no-moment': noMoment,
 };
 
@@ -8,7 +10,7 @@ export const configs = {
   recommended: {
     plugins: ['no-moment'],
     rules: {
-      'no-moment/no-moment': 'error' as const,
+      'no-moment/no-moment': 'error',
     }
   }
 };
